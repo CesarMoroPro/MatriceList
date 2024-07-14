bcfed7a4aef68367a383f074815950c6fb80bddd
César MORO <cesar.moro.dev@gmail.com>
2024-07-14 13:53:05 +0200

    Suppression et réinstall des node_modules ainsi que .next avec rebuild pour tenter de corriger l'erreur de déploiement. Jusqu'à maintenant, Vercel fail le déploiement à cause du fichier /lib/actions.ts qui pourtant n'existe plus, malgré les opérations de git push du projet après suppression

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,3 +3,9 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
     title: 'Login',
 }
+
+export default function LoginPage() {
+    return (
+        <h1>Page de connexion</h1>
+    )
+}
