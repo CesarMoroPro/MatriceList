be9d0f4ded7bab9e5eaa674e6f6eef75b4f728dc
César MORO <cesar.moro.dev@gmail.com>
2024-07-16 17:27:24 +0200

    Test commit après reset hard vers le commit bcfed7a

---
+++ src/app/lib/definitions.ts
@@ -0,0 +1,7 @@
+export type User = {
+    id: string;
+    firstname: string,
+    name: string;
+    email: string;
+    password: string;
+  };
---
+++ src/app/ui/button.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
+}
+
+export function Button({ children, className, ...rest }: ButtonProps) {
+
+    return (
+        <button
+            {...rest}
+            className={className}
+        >
+            {children}
+        </button>
+    );
+}
