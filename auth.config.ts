7246389c9a859ec1d17629c5fb40e84bd73613fc
César MORO <cesar.moro.dev@gmail.com>
2024-08-12 16:21:37 +0200

    Légère avancée sur le problème d'authentification après validation du formulaire de connexion. Problèmes a priori résolus : 1/ les données envoyées dans formData (objet de type FormData) sont extraites avec entries() afin de les rendre exploitable dans la fonction signIn() ; 2/ la redirection automatique vers '/login' a été empêchée avec 'redirect: false'. À partir de ce point, lors de la validation du formulaire, on entre bien dans la fonction signIn avec les données exploitables de la copie de formData, et on n'entre plus dans les cas du catch d'après les console.log. Il reste à réussir la redirection vers la bonne page

--- auth.config.ts
+++ auth.config.ts
@@ -2,17 +2,31 @@ import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
     pages: {
+        /* La propriété signIn indique à NextAuth.js quelle page doit être utilisée pour la phase d'authentification.
+        Lorsque l'utilisateur doit se connecter comme lorsqu'il tente d'accéder à une page protégée, NextAuth.js redirigera l'utilisateur vers la page '/login'
+        */
         signIn: '/login',
     },
+    debug: true,
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
+            /* Boolean qui récupère le pathname de l'URL contenu dans la requête (Rappel : pathname = morceau contenu dans URL entre https://nomsite.ext et les paramètres).
+            Si le pathname commence par '/dashboard', on veut forcément accéder au tableau de bord d'un user */
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
 
+            /* Donc si le user tente d'accéder à une page dont l'url commence par '/dashboard' */
             if (isOnDashboard) {
+                /* alors si l'utilisateur est connecté, return true et la redirection a lieu.
+                Cette redirection doit être gérée par le middleware de préférence */
                 if (isLoggedIn) return true;
-                return false; // Redirect unauthenticated users to login page
-            } else if (isLoggedIn) {
+                /* S'il n'est pas connecté, il est redirigé automatiquement sur login */
+                return false;
+            }
+            /* Si le user ne tente pas d'accéder à une page dont le pathname commence par '/dashboard',
+            et si l'utilisateur est connecté */
+            else if (isLoggedIn) {
+                /* Il peut être redirigé vers l'url contenu dans la requête */
                 return Response.redirect(new URL('/dashboard', nextUrl));
             }
             return true;
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 
 export default NextAuth(authConfig).auth;
-console.log(NextAuth(authConfig).auth);
 
 export const config = {
     matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
